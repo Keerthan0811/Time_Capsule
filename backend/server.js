c5ad5899d0c3cb3dfcd21e477732e5df68227296
Keerthan K V <keerthan11c@gmail.com>
2025-06-02 17:06:23 +0530

    updated docker

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,6 @@ app.use((req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
