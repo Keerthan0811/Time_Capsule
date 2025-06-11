ea77e48aea97bb0533045298e682b762a7084977
Keerthan K V <keerthan11c@gmail.com>
2025-06-12 03:56:36 +0530

    changed to env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://keerthan11c:pyAU2loXgXuMjDI3@devops.olckxah.mongodb.net/timecapsule?retryWrites=true&w=majority&appName=devops";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(uri);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`✅ MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("❌ MongoDB connection error:", error.message);
